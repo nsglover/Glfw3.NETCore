6d299e485e51c2f74e029d570cc90f0d5cbcc9ae
Nathaniel Glover <nathanielsglover@gmail.com>
2018-07-06 15:31:41 -0400

    Add simple window creation example

---
+++ Examples/src/WindowCreation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Glfw3;
+
+namespace Examples
+{
+    public class WindowCreation
+    {
+        public static void Main(string[] args)
+        {
+            Glfw.SetErrorCallback((error, description) => { Console.WriteLine($"GLFW Error {error}: {description}"); });
+            Glfw.Init();
+
+            var win = Glfw.CreateWindow(400, 400, "GLFW Window", IntPtr.Zero, IntPtr.Zero);
+            
+            while(Glfw.WindowShouldClose(win) == Glfw.False)
+            {
+                Glfw.PollEvents();
+            }
+        }
+    }
+}
